9a3d5cb2c4b1aae3502c4330bd642d5e9ef43dfe
yoshiki172365 <yoshiki-s@kvf.biglobe.ne.jp>
2021-02-11 22:46:52 +0900

    user-mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @apps = @user.apps
 
-    favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
-    @favorite_list = Post.find(favorites)     # postsテーブルから、お気に入り登録済みのレコードを取得
+    favorites = Favorite.where(user_id: current_user.id).pluck(:app_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
+    @favorite_list = App.find(favorites)     # postsテーブルから、お気に入り登録済みのレコードを取得
   end
 end
